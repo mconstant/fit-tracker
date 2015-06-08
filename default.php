6c61af3c2824b4b61ab4f3ebc23d419cf4ccea73
Matt Constant <mconstant3496@gmail.com>
2015-06-08 16:19:07 -0400

    Initial Commit

---
+++ default.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8 />
+	<title></title>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/master.css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header>
+		<h1>Fit Tracker</h1>
+		<span style="float: right; margin-right: 10px"><a href="#">Sign Out</a></span>
+	</header>
+	<sidebar>
+		<nav>
+			<ul>
+				<li><a href="#">Overview</a></li>
+				<li><a href="#">Log a Run</a></li>
+				<li><a href="#">View My Runs</a></li>
+			</ul>
+		</nav>
+	</sidebar>
+	<section class="content">
+		
+	</section>
+</body>
+</html>
